b736a41d849088b7e8bd34d8c84d0410373795ee
Mike Goren <mike@mortalwombat.net>
2018-11-16 23:18:11 -0800

    add tshirt info page template

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#welcome'
   get 'welcome', to: 'static_pages#welcome'
+  get 'tshirts', to: 'static_pages#tshirts'
   resources :orders, only: [:new, :create]
 end
