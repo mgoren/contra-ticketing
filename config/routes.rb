5043dc54a575316798ffb551e6ea6bef56628490
Mike Goren <mike@mortalwombat.net>
2020-03-13 20:35:44 -0700

    add link to donation site

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root 'orders#new'
+  root 'static_pages#unavailable'
+  # root 'orders#new'
   # get 'tshirts', to: 'static_pages#tshirts'
-  # get 'unavailable', to: 'static_pages#unavailable'
+  get 'unavailable', to: 'static_pages#unavailable'
   resources :orders, only: [:new, :create]
 end
